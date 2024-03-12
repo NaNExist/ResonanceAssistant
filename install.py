11ba6df597b14478775c380cedd9440444d944c5
NaNExist <138002947+NaNExist@users.noreply.github.com>
2024-03-12 21:07:52 +0800

    重构 dev

---
+++ install.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+import shutil
+import sys
+import json
+
+working_dir = Path(__file__).parent
+# version = len(sys.argv) > 1 and sys.argv[1] or "v0.0.1"
+
+if __name__ == "__main__":
+    version=input("version: ")
+    install_path = working_dir / Path("install/"+"ResonanceAssistant-"+version)
+    shutil.copytree(
+        working_dir / "deps" / "bin",
+        install_path,
+        ignore=shutil.ignore_patterns(
+            "*MaaDbgControlUnit*",
+            "*MaaThriftControlUnit*",
+            "*MaaWin32ControlUnit*",
+            "*MaaRpc*",
+        ),
+        dirs_exist_ok=True,
+    )
+    shutil.copytree(
+        working_dir / "deps" / "share" / "MaaAgentBinary",
+        install_path / "MaaAgentBinary",
+        dirs_exist_ok=True,
+    )
+
+    shutil.copytree(
+        working_dir / "assets" / "resource",
+        install_path,
+        dirs_exist_ok=True,
+    )
+    shutil.copy2(
+        working_dir / "assets" / "interface.json",
+        install_path,
+    )
+    shutil.copytree(
+        working_dir / "assets" /"MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn",
+        install_path / "resource" / "model" / "ocr",
+        dirs_exist_ok=True,
+    )
+    shutil.copy2(
+        working_dir / "README.md",
+        install_path,
+    )
+    shutil.copy2(
+        working_dir / "LICENSE",
+        install_path,
+    )
+
+    with open(install_path / "interface.json", "r", encoding="utf-8") as f:
+        interface = json.load(f)
+
+    interface["version"] = version
+
+    with open(install_path / "interface.json", "w", encoding="utf-8") as f:
+        json.dump(interface, f, ensure_ascii=False, indent=4)
